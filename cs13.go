fc69232aa172f6461f1f2204d86e245265f334a5
NNCZ12 <57839809+NNCZ12@users.noreply.github.com>
2019-11-23 22:20:49 +0700

    Update cs13.go

--- cs13.go
+++ cs13.go
@@ -1,3 +1,14 @@
 package main
 
 import "fmt"
+
+func main() {
+	fmt.Println("Khet")
+	fmt.Println("Com")
+	fmt.Println("Bam")
+	fmt.Println("Boat")
+	fmt.Println("Ice")
+	fmt.Println("Fang")
+	fmt.Println("Tan")
+	fmt.Println("Nan")
+	fmt.Println("Arm")
