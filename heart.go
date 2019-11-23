afb0f7f28edfbe4fcfe22d6409962853cf666cab
NNCZ12 <57839809+NNCZ12@users.noreply.github.com>
2019-11-23 22:33:14 +0700

    Update heart.go

--- heart.go
+++ heart.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	
+	{
+		fmt.Println("              **             **")
+		fmt.Println("          **      **      **     **")
+		fmt.Println("        **          *    *        **")
+		fmt.Println("      **               *            **")
+		fmt.Println("     **                              **")
 }
