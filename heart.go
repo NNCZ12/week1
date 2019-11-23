58bd5ff53d94bc49a56c2225ec9a10f03f0de7dc
NNCZ12 <57839809+NNCZ12@users.noreply.github.com>
2019-11-23 22:42:24 +0700

    Update heart.go

--- heart.go
+++ heart.go
@@ -3,23 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	{
-		fmt.Println("              **             **")
-		fmt.Println("          **      **      **     **")
-		fmt.Println("        **          *    *        **")
-		fmt.Println("      **               *            **")
-		fmt.Println("     **                              **")
-		fmt.Println("     **                              **")
-	    fmt.Println("     **                              **")
-	    fmt.Println("     **                              **")
-	    fmt.Println("      **                            **")
-	    fmt.Println("        **                        **")
-		fmt.Println("          **                    **")
-		fmt.Println("            **                **")
-	    fmt.Println("             **              **")
-	    fmt.Println("               **          **")
-	    fmt.Println("                 **      **")
-	    fmt.Println("                    *  *")
-	    fmt.Println("                     *")
-		
+
+	fmt.Println("              **             **")
+	fmt.Println("          **      **      **     **")
+	fmt.Println("        **          *    *        **")
+	fmt.Println("      **               *            **")
+	fmt.Println("     **                              **")
+	fmt.Println("     **                              **")
+	fmt.Println("     **                              **")
+	fmt.Println("     **                              **")
+	fmt.Println("      **                            **")
+	fmt.Println("        **                        **")
+	fmt.Println("          **                    **")
+	fmt.Println("            **                **")
+	fmt.Println("             **              **")
+	fmt.Println("               **          **")
+	fmt.Println("                 **      **")
+	fmt.Println("                    *  *")
+	fmt.Println("                     *")
+
 }
